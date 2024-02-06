f984b76ed0b3e83d97d09e529ea70dcb4b5f3073
lazar <HeidyLazaro>
2024-02-05 22:16:00 -0600

    Se agregaron las dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,10 @@ android {
 }
 
 dependencies {
-
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Retrofit with Scalar Converter
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     // Import the Compose BOM
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.activity:activity-compose:1.8.0")
