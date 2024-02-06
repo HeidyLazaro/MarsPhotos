9c7bd2d8c60a9da93a2ac88dc9ae46573e9403ce
EduardoLJ <edwardelric1291@gmail.com>
2024-02-06 04:30:02 -0600

    Pasos 4, 5 y 6 de Cómo agregar un repositorio y una DI manual (Error de instanciación).

---
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -0,0 +1,22 @@
+package com.example.marsphotos.data
+
+import com.example.marsphotos.network.MarsPhoto
+import com.example.marsphotos.network.MarsApiService
+
+/**
+ * Repository that fetch mars photos list from marsApi.
+ */
+interface MarsPhotosRepository {
+    /** Fetches list of MarsPhoto from marsApi */
+    suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+/**
+ * Network Implementation of Repository that fetch mars photos list from marsApi.
+ */
+class NetworkMarsPhotosRepository(
+    private val marsApiService: MarsApiService
+) : MarsPhotosRepository {
+    /** Fetches list of MarsPhoto from marsApi*/
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+}
